c5127e3b75d3f00d1837e824288b3c90c1474d22
Amit Salaria <amit.salaria@payu.in>
2022-03-29 13:29:39 +0530

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "PayUIndia-NativeOtpAssist",
+    platforms: [.iOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,25 +14,25 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "PayUIndia-PayUParams",url: "https://github.com/payu-intrepos/payu-params-iOS", from: "3.2.0"),
-        .package(name: "PayUIndia-NetworkReachability",url: "https://github.com/payu-intrepos/PayUNetworkReachability-iOS", from: "2.0.0"),
-        .package(name: "PayUIndia-Analytics",url: "https://github.com/payu-intrepos/PayUAnalytics-iOS", from: "2.1.0"),
-        .package(name: "PayUIndia-CrashReporter",url: "https://github.com/payu-intrepos/PayUCrashReporter-iOS", from: "2.0.0")
+        .package(name: "PayUIndia-PayUParams",url: "https://github.com/payu-intrepos/payu-params-iOS", from: "3.2.1"),
+        .package(name: "PayUIndia-NetworkReachability",url: "https://github.com/payu-intrepos/PayUNetworkReachability-iOS", from: "2.0.1"),
+        .package(name: "PayUIndia-Analytics",url: "https://github.com/payu-intrepos/PayUAnalytics-iOS", from: "2.1.1"),
+        .package(name: "PayUIndia-CrashReporter",url: "https://github.com/payu-intrepos/PayUCrashReporter-iOS", from: "2.0.1")
         
         
     ],
     targets: [
-            .binaryTarget(name: "PayUIndia-NativeOtpAssist", path: "./framework/PayUNativeOtpAssist.xcframework"),
-                .target(
-                    name: "PayUIndia-NativeOtpAssistTarget",
-                    dependencies: [
-                        .target(name: "PayUIndia-NativeOtpAssist", condition: .when(platforms: [.iOS])),
-                        .product(name: "PayUIndia-PayUParams", package: "PayUIndia-PayUParams"),
-                        "PayUIndia-Analytics",
-                        "PayUIndia-NetworkReachability",
-                        "PayUIndia-CrashReporter"
-                    ],
-                    path: "PayUIndia-NativeOtpAssistWrapper"
-                ),
-        ]
+        .binaryTarget(name: "PayUNativeOtpAssist", path: "./framework/PayUNativeOtpAssist.xcframework"),
+        .target(
+            name: "PayUIndia-NativeOtpAssistTarget",
+            dependencies: [
+                .product(name: "PayUIndia-PayUParams", package: "PayUIndia-PayUParams"),
+                "PayUIndia-Analytics",
+                "PayUIndia-NetworkReachability",
+                "PayUIndia-CrashReporter",
+                "PayUNativeOtpAssist"
+            ],
+            path: "PayUIndia-NativeOtpAssistWrapper"
+        ),
+    ]
 )
