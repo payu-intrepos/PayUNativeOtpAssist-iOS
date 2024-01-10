f4cd7647b7f965cfee45ae68da550f1edb73722d
Shubham Garg <shubham.garg@payu.in>
2024-01-10 23:28:13 +0530

    AmazonPay Secure Webview

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@
 
 import PackageDescription
 
-let VERSION_PARAM_KIT: PackageDescription.Version = "5.1.0"
+let VERSION_PARAM_KIT: PackageDescription.Version = "5.2.0"
 let VERSION_ANALYTICS_KIT: PackageDescription.Version = "3.0.0"
-let VERSION_CRASH_REPORTER: PackageDescription.Version = "2.1.0"
+let VERSION_CRASH_REPORTER: PackageDescription.Version = "3.0.0"
 let VERSION_NETWORK_REACHABILITY: PackageDescription.Version = "2.0.1"
 let VERSION_COMMON_UI: PackageDescription.Version = "1.2.0"
 let package = Package(
