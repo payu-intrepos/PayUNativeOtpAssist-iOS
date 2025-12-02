961ed5c05da692f7404040d964a4042fdaa77eed
Shubham Garg <91garg.shubham@gmail.com>
2025-12-03 00:39:58 +0530

    updating paramkit version

--- Package.swift
+++ Package.swift
@@ -3,11 +3,11 @@
 
 import PackageDescription
 
-let VERSION_PARAM_KIT: PackageDescription.Version = "6.6.0"
+let VERSION_PARAM_KIT: PackageDescription.Version = "7.0.0"
 let VERSION_ANALYTICS_KIT: PackageDescription.Version = "4.0.2"
 let VERSION_CRASH_REPORTER: PackageDescription.Version = "4.0.1"
 let VERSION_NETWORK_REACHABILITY: PackageDescription.Version = "2.1.1"
-let VERSION_COMMON_UI: PackageDescription.Version = "2.2.0"
+let VERSION_COMMON_UI: PackageDescription.Version = "2.3.2"
 
 let package = Package(
 
