4a0bd83b03b5d311596e9a5f14632962a9bf731e
mobilepayu <payumoneyapp@payumoney.com>
2026-01-12 17:14:45 +0530

    Updated frameworks

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let VERSION_PARAM_KIT: PackageDescription.Version = "7.0.0"
+let VERSION_PARAM_KIT: PackageDescription.Version = "7.0.1"
 let VERSION_ANALYTICS_KIT: PackageDescription.Version = "4.0.2"
 let VERSION_CRASH_REPORTER: PackageDescription.Version = "4.0.1"
 let VERSION_NETWORK_REACHABILITY: PackageDescription.Version = "2.1.1"
--- PayUIndia-NativeOtpAssist.podspec
+++ PayUIndia-NativeOtpAssist.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
 s.name                = "PayUIndia-NativeOtpAssist"
-s.version             = "5.0.0"
+s.version             = "5.0.1"
 s.license             = "MIT"
 s.homepage            = "https://github.com/payu-intrepos/PayUNativeOtpAssist-iOS"
 s.author              = { "PayUbiz" => "contact@payu.in"  }
